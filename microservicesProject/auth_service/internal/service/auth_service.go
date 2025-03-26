6b440ff96332a3793279036f5f702ab4dbb772a1
niktin06sash <150686206+niktin06sash@users.noreply.github.com>
2025-03-26 14:27:24 +0300

    Add files via upload

---
+++ microservicesProject/auth_service/internal/service/auth_service.go
@@ -0,0 +1,128 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"microservicesProject/auth_service/internal/erro"
+	"microservicesProject/auth_service/internal/model"
+	"microservicesProject/auth_service/internal/repository"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthService struct {
+	repo      repository.Authorization
+	validator *validator.Validate
+}
+
+func NewAuthService(repo repository.Authorization) *AuthService {
+	validator := validator.New()
+	return &AuthService{repo: repo, validator: validator}
+}
+
+type AuthenticationServiceResponse struct {
+	Success bool
+	UserId  uuid.UUID
+	Errors  interface{}
+}
+
+func (as *AuthService) Registrate(user *model.Person, ctx context.Context) *AuthenticationServiceResponse {
+	errorvalidate := validatePerson(as, user, true)
+	if errorvalidate != nil {
+		return &AuthenticationServiceResponse{Success: false, Errors: errorvalidate}
+	}
+	hashpass, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		hashmapa := make(map[string]interface{})
+		hashmapa["HashPass"] = erro.ErrorHashPass
+		return &AuthenticationServiceResponse{Success: false, Errors: err}
+	}
+	user.Password = string(hashpass)
+	userID := uuid.New()
+	user.Id = userID
+	response := as.repo.CreateUser(ctx, user)
+	if !response.Success {
+		return &AuthenticationServiceResponse{Success: false, Errors: response.Errors}
+	}
+	return &AuthenticationServiceResponse{Success: true, UserId: response.UserId}
+}
+func (as *AuthService) Authorizate(user *model.Person, ctx context.Context) *AuthenticationServiceResponse {
+	errorvalidate := validatePerson(as, user, false)
+	if errorvalidate != nil {
+		return &AuthenticationServiceResponse{Success: false, Errors: errorvalidate}
+	}
+	response := as.repo.GetUser(ctx, user.Email, user.Password)
+	if !response.Success {
+		return &AuthenticationServiceResponse{Success: false, Errors: response.Errors}
+	}
+	return &AuthenticationServiceResponse{Success: true, UserId: response.UserId}
+}
+func validatePerson(as *AuthService, user *model.Person, flag bool) map[string]interface{} {
+	personToValidate := *user
+	if !flag {
+		personToValidate.Name = "qwertyuiopasdfghjklzxcvbn"
+	}
+	err := as.validator.Struct(&personToValidate)
+	if err != nil {
+		validationErrors, ok := err.(validator.ValidationErrors)
+		if ok {
+			errors := make(map[string]interface{})
+			for _, err := range validationErrors {
+
+				switch err.Tag() {
+
+				case "email":
+					errors[err.Field()] = fmt.Sprint(erro.ErrorNotEmail)
+
+				case "min":
+					errv := fmt.Sprintf("%s is too short", err.Field())
+					errors[err.Field()] = fmt.Sprint(errv)
+
+				default:
+					errv := fmt.Sprintf("%s is Null", err.Field())
+					errors[err.Field()] = fmt.Sprint(errv)
+				}
+			}
+			return errors
+		}
+	}
+	return nil
+}
+
+var sessions = make(map[string]Session)
+
+type Session struct {
+	UserID    uuid.UUID
+	ExpiresAt time.Time
+}
+
+func (as *AuthService) GenerateSession(userID uuid.UUID) (string, time.Time) {
+	sessionID := uuid.New().String()
+	expiresAt := time.Now().Add(time.Hour * 24)
+	session := Session{
+		UserID:    userID,
+		ExpiresAt: expiresAt,
+	}
+
+	sessions[sessionID] = session
+	return sessionID, expiresAt
+}
+func (as *AuthService) CheckSession(sessionID string) *AuthenticationServiceResponse {
+	session, ok := sessions[sessionID]
+	if !ok {
+		return &AuthenticationServiceResponse{Success: false, UserId: uuid.Nil}
+	}
+	if time.Now().After(session.ExpiresAt) {
+
+		return &AuthenticationServiceResponse{Success: false, UserId: uuid.Nil}
+	}
+
+	return &AuthenticationServiceResponse{Success: true, UserId: session.UserID}
+}
+
+/*func (as *AuthService) DeleteSession(token string) error {
+	//return nil
+}*/
